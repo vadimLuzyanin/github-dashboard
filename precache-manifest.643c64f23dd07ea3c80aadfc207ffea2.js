25c2c867742bc3ccf4c380e0fbde7bb698afe031
Vadim Luzyanin <luzianin999@inbox.ru>
2020-06-22 16:55:53 +0300

    Updates

---
+++ precache-manifest.643c64f23dd07ea3c80aadfc207ffea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a145d99edb2aa223f52fe2c12efd9cf",
+    "url": "/github-dashboard/index.html"
+  },
+  {
+    "revision": "2c2ab3dee29c4a2f1894",
+    "url": "/github-dashboard/static/css/main.5ed34ac7.chunk.css"
+  },
+  {
+    "revision": "7e04990a32e0c7bb5539",
+    "url": "/github-dashboard/static/js/2.e65c9e8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-dashboard/static/js/2.e65c9e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c2ab3dee29c4a2f1894",
+    "url": "/github-dashboard/static/js/main.aae8996b.chunk.js"
+  },
+  {
+    "revision": "ad6480f23577cdc51850",
+    "url": "/github-dashboard/static/js/runtime-main.cb2b332b.js"
+  }
+]);
